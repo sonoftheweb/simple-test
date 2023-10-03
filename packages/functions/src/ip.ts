f704e942be82985a9a77c9812a7943baa4892509
Jacob Ekanem <prometheus6001@gmail.com>
2023-10-03 11:00:13 -0300

    Update to use name in response when needed.

--- packages/functions/src/ip.ts
+++ packages/functions/src/ip.ts
@@ -2,14 +2,21 @@ import { APIGatewayProxyHandlerV2 } from 'aws-lambda'
 
 export const handler: APIGatewayProxyHandlerV2 = async (event) => {
   const ip = event.requestContext.http.sourceIp
+  const name = event.queryStringParameters?.name
+
+  let response: { ip: string; greeting?: string } = { ip }
+
+  if (name) {
+    response.greeting = `${name}`
+  }
 
   return {
     statusCode: ip ? 200 : 400,
     headers: {
-      'x-hello-world': 'JE'
+      'x-hello-world': 'JE',
     },
     body: ip
-      ? JSON.stringify({ ip })
+      ? JSON.stringify(response)
       : JSON.stringify({ error: 'error-code-100' }),
   }
 }
